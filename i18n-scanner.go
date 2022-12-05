989aec8223c5dbedacc9d6ae59be0b4c6430ff83
greensea <gs@bbxy.net>
2022-12-05 16:36:02 +0800

    Initial commit

---
+++ i18n-scanner.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+var langArg string
+var langs []string
+var funcKeyword string
+var scanDir string
+var msgFile string
+
+// Total number of messages found
+var msgCount int
+
+type File map[string]Messages
+type Messages map[string]string
+
+// Save messages to path
+func (f File) Save(path string) error {
+	raw, err := json.MarshalIndent(f, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(path, raw, 0666)
+	return err
+}
+
+// Load messages from path. All messages will be flushed before load
+func (f File) Load(path string) error {
+	raw, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(raw, &f)
+	return err
+}
+
+// Add a new message. If the message translation is exists, the file is inact
+func (f File) Add(msg string) {
+	for _, v := range f {
+		_, ok := v[msg]
+		if ok != true {
+			v[msg] = ""
+		}
+	}
+
+}
+
+func (f File) AddLocale(locale string) {
+	_, ok := f[locale]
+	if ok == false {
+		f[locale] = make(Messages)
+	}
+}
+
+func main() {
+	flag.StringVar(&langArg, "l", "en,zh", "Language names")
+	flag.StringVar(&funcKeyword, "k", "_", "Translate function name, This arg will be a part of regular expression, if there are special chars you have to escape it manually")
+	flag.StringVar(&scanDir, "d", "", "Directory to scan")
+	flag.StringVar(&msgFile, "m", "messages.json", "Message file")
+
+	langs = strings.Split(langArg, ",")
+
+	flag.Usage = func() {
+		fmt.Printf("i18n-scanner (https://github.com/greensea/i18n-scanner)\n\n")
+		fmt.Printf("Usage: \n  i18n-scanner [OPTIONS] -d <DIR TO SCAN>\n\n")
+		fmt.Printf("Options:\n")
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if scanDir == "" {
+		flag.Usage()
+		fmt.Printf("\nError: missing -d argument\n")
+		os.Exit(0)
+	}
+
+	Scan()
+}
+
+func Scan() {
+	f := make(File)
+	f.Load(msgFile)
+
+	for _, lang := range langs {
+		f.AddLocale(lang)
+	}
+
+	ScanDir(f, scanDir)
+	fmt.Printf("Found %d messages\n", msgCount)
+
+	f.Save(msgFile)
+	fmt.Printf("Messages json file saved to %s\n", msgFile)
+}
+
+func ScanDir(f File, path string) {
+	entry, err := os.ReadDir(path)
+	if err != nil {
+		log.Printf("Unable to scan %s: %v\n", path, err)
+		return
+	}
+
+	for _, v := range entry {
+		subpath := fmt.Sprintf("%s/%s", path, v.Name())
+		if v.IsDir() {
+			ScanDir(f, subpath)
+		} else {
+			ScanFile(f, subpath)
+		}
+	}
+}
+
+func ScanFile(f File, path string) {
+	raw, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Printf("Unable to read file %s: %v\n", path, err)
+		return
+	}
+
+	fmt.Printf("Parsing %s\n", path)
+
+	msgs := Parse(string(raw), funcKeyword)
+	for _, v := range msgs {
+		f.Add(v)
+		msgCount++
+	}
+}
+
+func Parse(raw string, funcName string) []string {
+	var ret []string
+
+	// Match _('bla')
+	exp1 := `__\([ ]*"(.+)"[ ]*\)`
+	r1, err := regexp.Compile(exp1)
+	if err != nil {
+		fmt.Printf("Unable to compile regular expression %s\nCheck if there are special chars in function name parameter", exp1)
+		os.Exit(0)
+	}
+
+	// Match _("bla")
+	exp2 := `__\([ ]*'(.+)'[ ]*\)`
+	r2, err := regexp.Compile(exp2)
+	if err != nil {
+		fmt.Printf("Unable to compile regular expression %s\nCheck if there are special chars in function name parameter", exp1)
+		os.Exit(0)
+	}
+
+	ret1 := r1.FindAllStringSubmatch(raw, -1)
+	ret2 := r2.FindAllStringSubmatch(raw, -1)
+
+	for _, v := range ret1 {
+		if len(v) >= 2 {
+			ret = append(ret, v[1])
+		}
+	}
+
+	for _, v := range ret2 {
+		if len(v) >= 2 {
+			ret = append(ret, v[1])
+		}
+	}
+
+	return ret
+}
---
+++ i18n-scanner_test.go
@@ -0,0 +1,203 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestParse(t *testing.T) {
+	strs := Parse(sample, "__")
+	if len(strs) != 12 {
+		t.FailNow()
+	}
+}
+
+func TestFile(t *testing.T) {
+	f := make(File)
+	d := os.TempDir()
+	p := fmt.Sprintf("%s/i18n-scanner_test-%d.json", d, time.Now().UnixNano())
+
+	f.Load(p)
+	f.AddLocale("zh")
+	f.AddLocale("en")
+	f.Add("Test Message")
+	f.Add("测试翻译条目")
+	f.Save(p)
+
+	defer os.Remove(p)
+
+	f.Load(p)
+}
+
+var sample string = `<script setup>
+import {ref} from 'vue'
+</script>
+<script>
+let speeds = []
+for (let i =15; i < 32; i++) {
+    speeds.push({
+        hardness: 2**i - 1,
+        hardness_html: '2<sup>${i}</sup> - 1  <span class="text-gray-400"> == ${2**i - 1}</span>',
+        time_str: this.__("未计算"),
+    })
+}
+
+export default {
+    data() {
+        return {
+            activeName: "performance",
+            speeds: speeds,
+            isEvaling: {},
+            n: "9e471153ecd402c1779e21985cca2ef2df1ff567bc1767f559256f3f592f4f7669d34e7b959d259796307040f8fc98c2702049e48730c9a4dcb2e4d66d60b5f3",
+            x: "62bbf3bbbad2b415",
+        }
+
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        async startEval(row) {
+            let t = row.hardness
+            let worker = new Worker("vdfworker.js")
+            let that = this
+            let stime = (new Date()).getTime()
+
+            this.isEvaling[t] = true
+
+            worker.onmessage = msg => {
+                let d = msg.data;
+
+                if (d[0] == "eval_progress") {
+                    console.debug("Got VDF calculate progress", d[1])
+                    row.time_str = Math.round(d[1] * 100) + "%"
+                } else if (d[0] == "eval_result") {
+                    row.time_ms = (new Date()).getTime() - stime
+                    if (row.time_ms > 1000) {
+                        row.time_str = (row.time_ms / 1000).toFixed(1) + that.__(" 秒")
+                    } else {
+                        row.time_str = Math.round(row.time_ms) + that.__(" 毫秒")
+                    }
+                    this.isEvaling[t] = false
+                    worker.terminate()
+                } else {
+                    console.log(d);
+                }
+            }
+
+            worker.postMessage(['eval', this.x, t, this.n])
+
+//            worker.terminate()
+
+
+
+        },
+    },
+
+    computed: {
+
+    },
+}
+</script>
+
+
+<template>
+
+    <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick" tab-position="left">
+        <el-tab-pane :label="__(    '原理说明')" name="intro" class="intro">
+            
+            <!-- <p class="text-center">$y \equiv x ^ {2 ^ t} (\bmod n)$</p> -->
+            <p class="text-center">y = x<sup>2<sup>t</sup></sup> mod n</p>
+            
+        </el-tab-pane>
+
+        <el-tab-pane :label="__('API 列表')" name="api" class="api" >
+            <h2>wcaptcha(api_key)</h2>
+            
+            <highlightjs code='var w = new wcaptcha("YOUR_API_KEY");'></highlightjs>
+            
+            <highlightjs code='var w = new wcaptcha("YOUR_API_KEY");
+w.bind("#any-dom-selector");'></highlightjs>
+            <el-divider />
+
+            <h2>wcaptcha.prototype.prove()</h2>
+            <highlightjs code='var w = new wcaptcha("YOUR_API_KEY");
+w.prove().then(proof => {
+    console.log("proof is", prove);
+}");'></highlightjs>
+
+            <el-divider />
+            <h2>wcaptcha.prototype.getProblem()</h2>
+        </el-tab-pane>
+
+        <el-tab-pane label="自定义使用" name="advance" class="advance">
+
+            <highlightjs code="w = new wcaptcha(API_KEY);
+w.onprogress = (progress) => {
+    console.log('进度百分比:', progress * 100)
+}"></highlightjs>
+
+            <highlightjs code='w.prove().then(proof => {
+    formData = new FormData()
+    formData.append("wcaptcha-proof", proof)
+    formData.append("Your", "Form Data")
+    
+    // Submit your data...
+})
+            '></highlightjs>
+
+            
+
+        </el-tab-pane>
+
+        <el-tab-pane :label="__('速度测试')" name="performance" class="performance">
+
+            <el-table :data="speeds" class="mt-2 max-w-md" table-layout="auto">
+                <el-table-column :label="__('难度')">
+                    <template #default="scope">
+                        <span v-html="scope.row.hardness_html"></span>
+                    </template>
+                </el-table-column>
+                <el-table-column prop="time_str" :label="__('验证耗时')" />
+                <el-table-column prop="str" :label="__('测试')">
+                    <template #default="scope">
+                        <el-button @click="startEval(scope.row)" :loading="isEvaling[scope.row.hardness]" :disabled="isEvaling[scope.row.hardness]">
+                            <span v-if="(isEvaling[scope.row.hardness] == true)">{{__('正在计算')}}</span>
+                            <span v-else>{{__('开始测试')}}</span>
+                        </el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+
+        </el-tab-pane>
+
+        <el-tab-pane :label="__('常见问题')" name="qna" class="qna">
+ 
+        </el-tab-pane>
+
+    </el-tabs>
+
+</template>
+
+
+<style scoped>
+.intro p, el-ul, ol {
+    line-height: 1.5rem;
+    margin: 1em;
+    text-indent: 2em;
+}
+
+.api h2, .qna h2 {
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin-bottom: 0.5em;
+}
+.api p, .qna p, .advance p, .performance p {
+    margin-bottom: 0.5em;
+}
+</style>
+`
